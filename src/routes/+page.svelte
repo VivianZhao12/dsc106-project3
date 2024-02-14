d407357af5eec130adb9ab3e467f38a27828a964
Vivian Zhao <vivianzhao@Vivians-Air.attlocal.net>
2024-02-13 22:32:49 -0800

    add data files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+let tempData = [];
+
+ onMount(async () => {
+
+     const res = await fetch('county_data.csv'); 
+
+     const csv = await res.text();
+
+     tempData = d3.csvParse(csv, d3.autoType)
+
+     console.log(tempData);
+
+ });
+ </script>
